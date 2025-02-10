d51f4e9e446c0782dd17ca821d2df7e393181f63
kk <921232958@qq.com>
2025-02-10 17:27:29 +0800

    add prototype

--- main/src/main.cpp
+++ main/src/main.cpp
@@ -1,12 +1,14 @@
 #include <Demo/demo_singleton.h>
 #include <Demo/demo_factory.h>
 #include <Demo/demo_builder.h>
+#include <Demo/demo_prototype.h>
 
 int main()
 {
     // Demo_singleton::demo_v1();
     // Demo_Factory::demo_v1();
     // Demo_Factory::demo_v2();
-    Demo_Builder::demo_v1();
+    // Demo_Builder::demo_v1();
+    Demo_Prototype::demo_v1();
     return 0;
 }
