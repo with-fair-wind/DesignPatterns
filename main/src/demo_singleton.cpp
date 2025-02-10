10a3d646dffdcc9f78e5416c6613493dd000a0e7
kk <921232958@qq.com>
2025-02-10 15:35:10 +0800

    fix singleton

--- main/src/demo_singleton.cpp
+++ main/src/demo_singleton.cpp
@@ -4,7 +4,13 @@ namespace Demo_singleton
 {
     void demo_v1()
     {
+#ifdef Version_1
         Model_Singleton::Singleton &singleton = Model_Singleton::Singleton::getInstance();
         singleton.print();
+#elif defined(Version_2)
+        Model_Singleton::Singleton *singleton = Model_Singleton::Singleton::getInstance();
+        singleton->print();
+        delete Model_Singleton::Singleton::getInstance();
+#endif
     }
 } // namespace Demo_singleton
